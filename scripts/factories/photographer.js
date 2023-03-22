4400f2b7fd4c486278cd176507dbd15f34d0129f
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2023-03-22 12:39:22 +0100

    progression photographerFactory

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,39 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+  const { name, portrait, city, country, tagline, price } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/photographers/${portrait}`;
 
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
-    }
-    return { name, picture, getUserCardDOM }
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    const h2 = document.createElement("h2");
+    const taglineElement = document.createElement("p");
+
+    const countryElement = document.createElement("p");
+    const cityElement = document.createElement("p");
+    const localisationElement = document.createElement("p");
+    const priceElement = document.createElement("p");
+
+    img.setAttribute("src", picture);
+    img.setAttribute("alt", name); // Alternative image text
+
+    h2.textContent = name;
+    taglineElement.textContent = tagline;
+    countryElement.textContent = country;
+    cityElement.textContent = city;
+
+    localisationElement.textContent = `${city}, ${country}`;
+
+    priceElement.textContent = `${price}€/jour`;
+
+    article.appendChild(img);
+    article.appendChild(h2);
+    article.appendChild(localisationElement);
+    article.appendChild(taglineElement);
+    article.appendChild(priceElement);
+
+    return article;
+  }
+
+  return { name, picture, getUserCardDOM };
 }
