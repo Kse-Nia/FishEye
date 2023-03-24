892e26a7b7dc92d15b7f610eb4677029f2714cfb
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2023-03-24 14:21:37 +0100

    update home

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -40,5 +40,5 @@ function photographerFactory(data) {
     return article;
   }
 
-  return { name, picture, getUserCardDOM };
+  return { name, picture, city, country, tagline, price, getUserCardDOM };
 }
