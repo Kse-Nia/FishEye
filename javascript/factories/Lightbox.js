43cf77cdf225d3c2f1c32f58949cfdac07754fa2
Ksenia <73356609+Kse-Nia@users.noreply.github.com>
2023-05-29 22:46:53 +0200

    lightbox - working; add accessibility

--- javascript/factories/Lightbox.js
+++ javascript/factories/Lightbox.js
@@ -1,133 +1,17 @@
-/* class Lightbox {
-  constructor(media, currentIndex) {
-    this.media = media;
-    this.currentIndex = currentIndex === undefined ? 0 : currentIndex;
-    this.createLightbox();
-    this.openBoxModal();
-    console.log("box current index", currentIndex);
-  }
-  createLightbox() {
-    this.$lightbox = document.createElement("div");
-    this.$lightbox.classList.add("lightbox");
-    //     this.$lightbox.innerHTML = `
-    <div class="lightbox__container" aria-label="image closeup view">
-    <div class="lightbox__container__media">
-      <div class="lightbox__container__media__image">
-        <img src="./assets/icons/prev.svg" alt="prev" class="prev">
-      </div>
-      <div class="lightbox-content_media">
-        <img class="lightbox__image lightboxMediaOpen" src="./assets/Media/930/Sport_Next_Hold.jpg" tabindex="0" alt="media - Climber">
-      </div>
-      <div class="lightbox__container__buttons">
-        <img src="./assets/icons/close.svg" alt="close" class="close">
-        <img src="./assets/icons/next.svg" alt="next" class="next">
-      </div>
-    </div>
-    <p class="lightbox__container__media_name" id="lightbox_media_name">Climber</p>
-  </div>
-    `; //
-
-    this.$lightbox.innerHTML = `
-    <div class= "lightbox__container lightbox" aria-label="image closeup view">
-    <div class="lightbox-content">
-      <div class="lightbox-content-media"> </div>
-      <div class="lightbox-controls">
-      <img src="./assets/icons/prev.svg" alt="Previous" class="lightbox-prev" />
-        <img src="./assets/icons/next.svg" alt="Next" class="lightbox-next" />
-        <img src="./assets/icons/close.svg" alt="Close" class="lightbox-close" />
-      </div>
-    </div>
-  </div>
-  `;
-    document.body.appendChild(this.$lightbox);
-
-    this.$prev = this.$lightbox.querySelector(".prev");
-    this.$next = this.$lightbox.querySelector(".next");
-    this.$close = this.$lightbox.querySelector(".lightbox-close");
-
-    this.$prev.addEventListener("click", () => this.showPrev());
-    this.$next.addEventListener("click", () => this.showNext());
-    this.$close.addEventListener("click", () => this.close());
-  }
-  openBoxModal() {
-    const mediaItem = this.media[this.currentIndex];
-    const mediaContainer = this.$lightbox.querySelector(
-      ".lightbox-content-media"
-    );
-    mediaContainer.innerHTML = "";
-
-    console.log(mediaItem);
-
-    let mediaElement;
-    if (mediaItem.video) {
-      mediaElement = document.createElement("iframe");
-      mediaElement.src = `./assets/Media/${mediaItem.photographerId}/${mediaItem.video}`;
-      mediaElement.setAttribute("controls", true);
-      mediaElement.classList.add("lightbox__video");
-    } else {
-      mediaElement = document.createElement("img");
-      mediaElement.classList.add("lightbox__image");
-      mediaElement.src = `./assets/Media/${mediaItem.photographerId}/${mediaItem.image}`;
-    }
-    mediaElement.classList.add("lightboxMediaOpen");
-    mediaElement.setAttribute("tabindex", "0");
-    mediaElement.setAttribute("alt", `media - ${mediaItem.title}`);
-    mediaContainer.appendChild(mediaElement);
-
-    const title = mediaElement.createElement("p");
-    title.classList.add("lightbox__container__media_name");
-    title.setAttribute("id", "lightbox_media_name");
-    title.textContent = mediaItem.title;
-    mediaContainer.appendChild(title);
-   
-  }
-  openBox() {
-    this.$lightbox.style.display = "flex";
-  }
-  close() {
-    this.$lightbox.style.display = "none";
-  }
-  showPrev() {
-    this.currentIndex--;
-    if (this.currentIndex < 0) {
-      this.currentIndex = this.media.length - 1;
-    }
-    this.openBoxModal();
-  }
-  showNext() {
-    this.currentIndex++;
-    if (this.currentIndex >= this.media.length) {
-      this.currentIndex = 0;
-    }
-    this.openBoxModal();
-  }
-}
- */
-
+/* eslint-disable no-unused-vars */
 class Lightbox {
   constructor(media, currentIndex) {
     this.media = media;
     this.currentIndex = currentIndex === undefined ? 0 : currentIndex;
     this.createLightbox();
     this.openBoxModal();
-    console.log("box current index", currentIndex);
+
+    window.addEventListener("keydown", (e) => this.accessKey(e));
   }
   createLightbox() {
     this.$lightbox = document.createElement("div");
     this.$lightbox.classList.add("lightbox");
 
-    /*     this.$lightbox.innerHTML = `
-  <div class= "lightbox" aria-label="image closeup view">
-  <div class="lightbox-content">
-    <div class="lightbox-content-media"> </div>
-    <div class="lightbox-controls">
-      <img src="./assets/icons/prev.svg" alt="Previous" class="lightbox-prev" />
-      <img src="./assets/icons/next.svg" alt="Next" class="lightbox-next" />
-      <img src="./assets/icons/close.svg" alt="Close" class="lightbox-close" />
-    </div>
-  </div>
-</div>
-`; */
     this.$lightbox.innerHTML = `
 <div class="lightbox" aria-label="image closeup view">
   <div class="lightbox-content">
@@ -138,7 +22,7 @@ class Lightbox {
     </div>
     <div class="lightbox-content-media"></div>
     <div class="lightbox_controlers">
-        <button>
+        <button class="lightbox_button">
           <img
             src="./assets/icons/close.svg"
             alt="Close"
@@ -157,6 +41,8 @@ class Lightbox {
 
     this.$prev = this.$lightbox.querySelector(".lightbox-prev");
     this.$next = this.$lightbox.querySelector(".lightbox-next");
+    this.$prev.setAttribute("aria-label", "Previous");
+    this.$next.setAttribute("aria-label", "Next");
     this.$close = this.$lightbox.querySelector(".lightbox-close");
 
     this.$prev.addEventListener("click", () => this.showPrev());
@@ -169,8 +55,6 @@ class Lightbox {
       ".lightbox-content-media"
     );
     mediaContainer.innerHTML = "";
-
-    console.log(mediaItem);
 
     let mediaElement;
     if (mediaItem.video) {
@@ -188,8 +72,8 @@ class Lightbox {
     mediaElement.setAttribute("alt", `media - ${mediaItem.title}`);
     mediaContainer.appendChild(mediaElement);
 
-    const title = document.createElement("p"); // updated from mediaElement.createElement to document.createElement
-    title.classList.add("lightbox__container__media_name");
+    const title = document.createElement("p");
+    title.classList.add("lightbox__media_name");
     title.setAttribute("id", "lightbox_media_name");
     title.textContent = mediaItem.title;
     mediaContainer.appendChild(title);
@@ -213,5 +97,17 @@ class Lightbox {
       this.currentIndex = 0;
     }
     this.openBoxModal();
+  }
+  // Accessibilty keyboard navigation
+  accessKey(e) {
+    if (e.key === "Escape") {
+      this.close();
+    }
+    if (e.key === "ArrowLeft" || e.key === 37) {
+      this.showPrev();
+    }
+    if (e.key === "ArrowRight" || e.key === 39) {
+      this.showNext();
+    }
   }
 }
